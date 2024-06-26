5b973b338876115893af9c78bed51fdf73ec72c5
souravroy <roy35-909@diu.edu.bd>
2024-06-27 00:19:30 +0600

    added slider model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from lottery.models import Lottery
+
+class Slider(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True,blank=True)
+    image = models.ImageField(upload_to='Slider/')
+    competition = models.ForeignKey(Lottery, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    def __str__(self) -> str:
+        return self.name
+    
+
+
--- home/views.py
+++ home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import redirect, render
 from django.utils import timezone
 from datetime import timedelta
 from django.core.serializers import serialize
-from lottery.models import Category, Lottery
+from lottery.models import Category
+from .models import Slider
 from order.models import Winner
 import json
 
@@ -10,14 +11,16 @@ def home(request):
 
     catagories = Category.objects.all()
     three_days_after = timezone.now() + timedelta(days=3)
-    lottery = Lottery.objects.all().order_by('draw_in')[:5]
+    lottery = Slider.objects.filter(is_active = True).order_by('-id')
     lottery_json = data = []
 
     for item in lottery:
         lottery_json.append({
-            'id': item.id,
+            'id': item.competition.id,
             'name': item.name,
+            'des' : item.description,
             'image': request.build_absolute_uri(item.image.url) if item.image else None
+            
         })
     lottery_json = json.dumps(lottery_json)
     return render(request, 'index.html', context={'catagories':catagories,'lottery_json':lottery_json})
